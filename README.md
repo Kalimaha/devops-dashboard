8a395a51c6bef67c3b991f492eab111646c45eef
Guido Barbaglia <guido@vinomofo.com>
2020-08-01 22:07:18 +1000

    Return pull requests as JSON

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # DevOps Dashboard
+
+About Netlify CLI
+Install CLI: npm install netlify-cli -g
+Init CLI: netlify init
+List functions: netlify functions:list
+Invoke function
+Run netlify dev in a separate tab, then
+netlify functions:invoke stock-great-wines --identity, or
+curl -X POST "http://localhost:8888/.netlify/functions/stock-great-wines" -d "{'spam': 'eggs'}"
--- functions/vm-github-pull-requests.go
+++ functions/vm-github-pull-requests.go
@@ -1,14 +1,19 @@
 package main
 
 import (
+	"encoding/json"
+	"fmt"
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
 )
 
 func handler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+	pullRequests := PullRequests("vinomofo")
+	body, _ := json.Marshal(pullRequests)
+	fmt.Printf(string(body))
 	return events.APIGatewayProxyResponse{
 		StatusCode: 200,
-		Body:       "Hello AWS Lambda and Netlify",
+		Body:       string(body),
 	}, nil
 }
 
