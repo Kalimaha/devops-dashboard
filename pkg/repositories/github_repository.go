496c457a79b7c664713f1a82f50de1c5e32a993b
Guido Barbaglia <guido@vinomofo.com>
2020-08-09 13:41:27 +1000

    Recover from panic

--- pkg/repositories/github_repository.go
+++ pkg/repositories/github_repository.go
@@ -2,7 +2,6 @@ package repositories
 
 import (
 	"context"
-	"fmt"
 	"github.com/google/go-github/github"
 	"golang.org/x/oauth2"
 	"os"
@@ -35,17 +34,12 @@ type Commit struct {
 }
 
 func CompareCommits(repositoryName string, head string, tail string) (commits []Commit) {
-	fmt.Printf("\tGH-REPO: repositoryName -> %s\n", repositoryName)
-	fmt.Printf("\tGH-REPO: repositoryName -> %s\n", head)
-	fmt.Printf("\tGH-REPO: repositoryName -> %s\n", tail)
-	// client := githubClient()
-	// commitsComparison, _, _ := client.Repositories.CompareCommits(context.Background(), "vinomofo", repositoryName, head, tail)
+	client := githubClient()
+	commitsComparison, _, _ := client.Repositories.CompareCommits(context.Background(), "vinomofo", repositoryName, head, tail)
 
-	// fmt.Printf("\tGH-REPO: commits -> %v\n", len(commitsComparison.Commits))
-	// for _, commit := range commitsComparison.Commits {
-	// 	fmt.Printf("\tGH-REPO: CONVERT -> %v\n", commit)
-	// 	commits = append(commits, buildCommit(*commit))
-	// }
+	for _, commit := range commitsComparison.Commits {
+		commits = append(commits, buildCommit(*commit))
+	}
 
 	return commits
 }
@@ -54,12 +48,7 @@ func PullRequests(repositoryName string) (pullRequests []PullRequest) {
 	client := githubClient()
 
 	opts := &github.PullRequestListOptions{"open", "", "", "created", "desc", github.ListOptions{PerPage: 15}}
-	githubPullRequests, _, err := client.PullRequests.List(context.Background(), "vinomofo", repositoryName, opts)
-
-	if err != nil {
-		fmt.Printf("Problem in getting pull requests: %v", err)
-		os.Exit(1)
-	}
+	githubPullRequests, _, _ := client.PullRequests.List(context.Background(), "vinomofo", repositoryName, opts)
 
 	for _, value := range githubPullRequests {
 		reviews := fetchReviews(client, repositoryName, *value.Number)
@@ -73,12 +62,7 @@ func PullRequests(repositoryName string) (pullRequests []PullRequest) {
 func fetchReviews(client *github.Client, repositoryName string, pullRequestNumber int) []Review {
 	reviews := make([]Review, 0)
 	opt := &github.ListOptions{PerPage: 10}
-	githubReviews, _, err := client.PullRequests.ListReviews(context.Background(), "vinomofo", repositoryName, pullRequestNumber, opt)
-
-	if err != nil {
-		fmt.Printf("Problem in getting reviews: %v", err)
-		os.Exit(1)
-	}
+	githubReviews, _, _ := client.PullRequests.ListReviews(context.Background(), "vinomofo", repositoryName, pullRequestNumber, opt)
 
 	for _, githubReview := range githubReviews {
 		review := buildReview(*githubReview)
@@ -90,13 +74,8 @@ func fetchReviews(client *github.Client, repositoryName string, pullRequestNumbe
 
 func githubClient() (client *github.Client) {
 	githubToken := os.Getenv("GITHUB_TOKEN")
-	fmt.Printf("\tgithubClient -> githubToken? %v\n", githubToken)
-	
 	tokenService := oauth2.StaticTokenSource(&oauth2.Token{AccessToken: githubToken})
-	fmt.Printf("\tgithubClient -> tokenService? %v\n", tokenService)
-	
 	tokenClient := oauth2.NewClient(context.Background(), tokenService)
-	fmt.Printf("\tgithubClient -> tokenClient? %v\n", tokenClient)
 
 	return github.NewClient(tokenClient)
 }
@@ -125,10 +104,42 @@ func buildReview(githubPullRequestReview github.PullRequestReview) Review {
 }
 
 func buildCommit(commit github.RepositoryCommit) Commit {
-	fmt.Printf("\tCOMMIT %+v\n", commit)
 	return Commit{
-		Message:     *commit.Commit.Message,
-		AuthorLogin: *commit.Author.Login,
-		AuthorURL:   *commit.Author.HTMLURL,
+		Message:     message(commit),
+		AuthorLogin: authorLogin(commit),
+		AuthorURL:   authorURL(commit),
 	}
+}
+
+func message(commit github.RepositoryCommit) (output string) {
+	defer func() {
+		if r := recover(); r != nil {
+			output = ""
+		}
+	}()
+
+	output = *commit.Commit.Message
+	return output
+}
+
+func authorLogin(commit github.RepositoryCommit) (output string) {
+	defer func() {
+		if r := recover(); r != nil {
+			output = ""
+		}
+	}()
+
+	output = *commit.Author.Login
+	return output
+}
+
+func authorURL(commit github.RepositoryCommit) (output string) {
+	defer func() {
+		if r := recover(); r != nil {
+			output = ""
+		}
+	}()
+
+	output = *commit.Author.HTMLURL
+	return output
 }
