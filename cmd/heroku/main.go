2746f8972bcc3466257971c807ee831c3aef9889
Guido Barbaglia <guido@vinomofo.com>
2020-08-09 13:45:52 +1000

    Cleanup

--- cmd/heroku/main.go
+++ cmd/heroku/main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"encoding/json"
-	"fmt"
 	"github.com/Kalimaha/devops-dashboard/pkg/repositories"
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
@@ -10,21 +9,10 @@ import (
 
 func herokuHandler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
 	githubRepositoryName, _ := request.QueryStringParameters["repositoryName"]
-	fmt.Printf("githubRepositoryName %s\n", githubRepositoryName)
-
 	herokuRepositoryName := herokuRepositoryName(githubRepositoryName)
-	fmt.Printf("herokuRepositoryName %s\n", herokuRepositoryName)
-
+	
 	releases := repositories.ListReleasesFor(herokuRepositoryName)
-	fmt.Printf("RELEASES %+v\n", releases)
-	fmt.Printf("RELEASES %+v\n", len(releases))
-	fmt.Printf("RELEASES 0 %+v\n", releases[0])
-	fmt.Printf("RELEASES 0 %+v\n", releases[0].CommitID)
-	fmt.Printf("RELEASES 1 %+v\n", releases[1])
-	fmt.Printf("RELEASES 1 %+v\n", releases[1].CommitID)
-
 	commits := repositories.CompareCommits(githubRepositoryName, releases[1].CommitID, releases[0].CommitID)
-	fmt.Printf("COMMITS %+v\n", commits)
 
 	body, _ := json.Marshal(commits)
 	return events.APIGatewayProxyResponse{
